4b9ced7d31472050943f7a34d7d2bf554b868ae6
shanndrea <150647755+shanndrea@users.noreply.github.com>
2023-12-03 00:26:32 +0700

    Add files via upload

---
+++ PRAKTIKUM/PERTEMUAN5/02 Akses DOM/js/main.js
@@ -0,0 +1,23 @@
+function myUniversal() {
+  document.querySelector("*").style.backgroundColor = "red";
+}
+
+function myType() {
+  document.querySelector("#logo").style.backgroundColor = "blue";
+}
+
+function myClass() {
+  document.querySelector(".section-c").style.backgroundColor = "yellow";
+}
+
+function myId() {
+  document.querySelector("#logo").style.backgroundColor = "blue";
+}
+
+function myAttribute() {
+  document.querySelector("a[target]").style.backgroundColor = "red";
+}
+
+function myGroup() {
+  document.querySelector("h2, h1").style.backgroundColor = "red";
+}
